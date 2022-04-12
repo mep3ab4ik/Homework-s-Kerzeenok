69a0968ce15a87f335694027317f995c5c8ae447
Nikita <kerzeenok.nikita@gmail.com>
2022-04-12 22:59:16 +0300

    New homework decorator

---
+++ Lesson 10/authenticator.py
@@ -0,0 +1,103 @@
+import os.path
+from datetime import datetime
+from exceptions import AuthorizationError, RegistrationError
+
+
+class Authenticator:
+    """Класс аутентификации пользователя."""
+
+
+    def __init__(self):
+
+        self.login: str | None = None
+        self._password: str | None = None
+        self.errors_count: int = 0
+        self.last_success_login_at: datetime | None = None
+
+        # Проверка на наличия файла 'auth.txt'
+
+        if self._is_auth_file_exist():
+            self._read_auth_file()
+
+
+
+
+    def _is_auth_file_exist(self) -> bool:
+        """Метод, который проверяет на наличия файла.
+
+        Если файл существует, то возвращает True иначе Else.
+        """
+
+        return os.path.exists('auth.txt')
+
+
+    def _read_auth_file(self):
+        """Метод считывания данных.
+
+        Метод считывает данные из файла и перезаписываем их в переменные.
+        """
+
+        with open('auth.txt') as f:
+            self.login = f.readline().strip()
+            self._password = f.readline().strip()
+            self.last_success_login_at = datetime.fromisoformat(f.readline().strip())
+            self.errors_count = int(f.readline().strip())
+
+
+    def authorize(self, login, password):
+        """Метод авторизации пользователя
+
+        Проверяет на наличие переменной 'login'.
+        Сравнивает введенные логин и пароль, с логином и паролем из файла.
+        """
+
+        if not self.login:
+            self.errors_count += 1
+            raise AuthorizationError("You haven't registered.")
+
+        if not login:
+            self.errors_count += 1
+            raise AuthorizationError("The login field cannot be empty.")
+
+        if login == self.login and password == self._password:
+            self._update_auth_file()
+        else:
+            self.errors_count += 1
+            raise AuthorizationError("The username or password is incorrect")
+
+
+
+    def _update_auth_file(self):
+        """Метод обновление данных в файле.
+
+        В файле обновляется время и количество ошибок.
+        """
+
+        with open("auth.txt", "w") as f:
+            f.write(f"{self.login}\n")
+            f.write(f"{self._password}\n")
+            self.last_success_login_at = datetime.utcnow()
+            f.write(f"{self.last_success_login_at.isoformat()}\n")
+            f.write(f"{self.errors_count}")
+
+
+    def registrate(self, login, password):
+        """Метод регистрации пользователя.
+
+        Если файл существует, то выводит ошибку.
+        Если нет, то создаем файл и записываем туда данные.
+
+        """
+        if self.login:
+            self.errors_count += 1
+            raise RegistrationError("You are already a registered user.")
+
+        if not login:
+            self.errors_count += 1
+            raise RegistrationError("The login field cannot be empty.")
+
+        self.login = login
+        self._password = password
+        self._update_auth_file()
+
+
