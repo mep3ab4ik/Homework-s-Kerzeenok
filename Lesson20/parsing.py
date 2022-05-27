184ab33af68c7f5d83a93da7eec7917467c28294
Nikita <kerzeenok.nikita@gmail.com>
2022-05-27 18:12:14 +0300

    add comment

--- Lesson20/parsing.py
+++ Lesson20/parsing.py
@@ -20,26 +20,36 @@ class Info:
 
 
 def main():
+
+    # Передаем наш url. В headers добавляем User-Agent для получение ответа 200
     r = requests.get(
         url=url,
         headers=headers
     )
 
+    # очищаем от html кода и оставляем все чистое
     soup = BeautifulSoup(r.text, 'html.parser')
 
+    # ищем данные в блок div, который имеет класс нужный нам класс
     all_soup = soup.find_all('div', class_='t517__sectioninfowrapper')
     topic = []
 
+    # Бегаем по блоку
     for i in all_soup:
+        # Ищем блок div c этим классом
         title = i.find('div', class_='t-name')
+        # Ищем все атрибуты li
         text = i.find_all('li')
         text_info = []
 
+        # Бегаем по атрибутам li. Забираем из них текст и очищаем от пробелов( в некоторых местах они есть)
         for t in text:
             text_info.append(t.text.strip())
-
-        topic.append(asdict(Info(title.text.strip(), text_info)))
+        # Добавляем в строку с помощью asdict
+        topic.append(asdict(Info(title.text, text_info)))
+    # Возвращаем json. ensure_ascii в False, т.к ensure_ascii в True возвращает символы не ASCII
     return json.dumps(topic, indent=0, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     print(main())
